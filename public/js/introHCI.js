a182fe826cc5074cffb03620b85d722607d67984
Anders Geitle <andersgeitle@gmail.com>
2017-02-15 23:38:07 -0800

    added ajax

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,8 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -27,6 +29,22 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, callBack);
+
+	console.log("/project/"+idNumber);
+}
+
+function callBack(result) {
+	console.log(result);
+	var projectHTML = 
+		"<img src="+result["image"]+" class='detailsImage'></img>"+
+		"<h3>"+result["title"]+"</h3>"+
+		"<h4>"+result["date"]+"</h4>"+
+		result["summary"];
+
+
+	$("#project" + result["id"] + " .details").html(projectHTML);
 }
 
 /*
@@ -34,5 +52,15 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette", changeColor);
 	console.log("User clicked on color button");
+}
+
+function changeColor(result) {
+	var colors = result["colors"]["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
